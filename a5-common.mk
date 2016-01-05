0bb89bc634df69847de1d763f9489e215fc884f9
Ethan Chen <intervigil@gmail.com>
2016-01-05 14:31:24 -0800

    a5-common: Use Snapdragon camera
    Change-Id: I962356910e54a0f20692bab49fb1a2b289c85762

--- a5-common.mk
+++ a5-common.mk
@@ -92,6 +92,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
+# Camera
+PRODUCT_PACKAGES += \
+    Snap
+
 # Filesystem management tools
 PRODUCT_PACKAGES += \
     make_ext4fs \
