33683d75b443aa9485ffb166ac01cf44bbce9e5d
Bruno Martins <bgcngm@gmail.com>
2016-05-08 00:24:10 +0100

    a5-common: Disable clang build and remove RIL shim
    * Turns out that the culprit for the rild crashes we were
      experiencing was clang
    * After disabling it we no longer need to shim RIL
    
    Change-Id: Ic23ab20ccfd8cadb16fcf3eac47bd489128b1404

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -31,7 +31,6 @@ TARGET_NO_BOOTLOADER := true
 # Platform
 TARGET_BOARD_PLATFORM := msm8226
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno305
-USE_CLANG_PLATFORM_BUILD := true
 
 # Architecture
 TARGET_ARCH := arm
